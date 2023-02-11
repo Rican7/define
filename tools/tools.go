6155906e6f6b8b3bf4d4f892c21b65a48051b084
Trevor Suarez <rican7@gmail.com>
2023-02-10 17:54:39 -0700

    Adding a tools sub-module
    Inspired by the Go wiki, but also by Uber's approach:
    
     - uber-go/multierr#38
     - uber-go/zap#914

---
+++ tools/tools.go
@@ -0,0 +1,19 @@
+//go:build tools
+// +build tools
+
+// Package tools provides tools for development.
+//
+// It follows the pattern set-forth in the wiki:
+//   - https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+//   - https://github.com/go-modules-by-example/index/tree/4ea90b07f9/010_tools
+//
+// Copyright © 2023 Trevor N. Suarez (Rican7)
+package tools
+
+import (
+	// Tools for development
+	_ "github.com/mitchellh/gox"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+)
