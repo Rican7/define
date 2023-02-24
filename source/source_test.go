3f9085a1b272ba293924491d2ffed8e173c6aabd
Trevor Suarez <rican7@gmail.com>
2023-02-23 17:34:11 -0700

    Adding tests for the new sorting behaviors

--- source/source_test.go
+++ source/source_test.go
@@ -2,7 +2,179 @@
 
 package source
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDictionaryResults_IsSortedForPrimaryResult(t *testing.T) {
+	for testName, testData := range map[string]struct {
+		results DictionaryResults
+		word    string
+		want    bool
+	}{
+		"nil": {
+			results: nil,
+			word:    "",
+			want:    true,
+		},
+		"empty": {
+			results: DictionaryResults{},
+			word:    "",
+			want:    true,
+		},
+		"single result and sorted": {
+			results: DictionaryResults{{Word: "test"}},
+			word:    "test",
+			want:    true,
+		},
+		"multi results and sorted": {
+			results: DictionaryResults{
+				{Word: "test"},
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: true,
+		},
+		"single results and no match": {
+			results: DictionaryResults{{Word: "nah"}},
+			word:    "test",
+			want:    false,
+		},
+		"multi results and no match": {
+			results: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: false,
+		},
+		"multi results and not sorted": {
+			results: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "test"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: false,
+		},
+	} {
+		t.Run(testName, func(t *testing.T) {
+			if got := testData.results.IsSortedForPrimaryResult(testData.word); got != testData.want {
+				t.Errorf("IsSortedForPrimaryResult returned wrong value. Got %#v. Want %#v.", got, testData.want)
+			}
+		})
+	}
+}
+
+func TestDictionaryResults_SortForPrimaryResult(t *testing.T) {
+	for testName, testData := range map[string]struct {
+		results DictionaryResults
+		word    string
+		want    DictionaryResults
+	}{
+		"nil": {
+			results: nil,
+			word:    "",
+			want:    nil,
+		},
+		"empty": {
+			results: DictionaryResults{},
+			word:    "",
+			want:    DictionaryResults{},
+		},
+		"single result and sorted": {
+			results: DictionaryResults{{Word: "test"}},
+			word:    "test",
+			want:    DictionaryResults{{Word: "test"}},
+		},
+		"multi results and sorted": {
+			results: DictionaryResults{
+				{Word: "test"},
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: DictionaryResults{
+				{Word: "test"},
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+		},
+		"single results and no match": {
+			results: DictionaryResults{{Word: "nah"}},
+			word:    "test",
+			want:    DictionaryResults{{Word: "nah"}},
+		},
+		"multi results and no match": {
+			results: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+		},
+		"multi results and not sorted with match in middle": {
+			results: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "test"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+			word: "test",
+			want: DictionaryResults{
+				{Word: "test"},
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+		},
+		"multi results and not sorted with match at end": {
+			results: DictionaryResults{
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+				{Word: "test"},
+			},
+			word: "test",
+			want: DictionaryResults{
+				{Word: "test"},
+				{Word: "nah"},
+				{Word: "nope"},
+				{Word: "def not"},
+				{Word: "pls stop"},
+			},
+		},
+	} {
+		t.Run(testName, func(t *testing.T) {
+			testData.results.SortForPrimaryResult(testData.word)
+			if !reflect.DeepEqual(testData.results, testData.want) {
+				t.Errorf("SortForPrimaryResult returned wrong value. Sorted %#v. Want %#v.", testData.results, testData.want)
+			}
+		})
+	}
+}
 
 func TestPronunciations_String(t *testing.T) {
 	for testName, testData := range map[string]struct {
